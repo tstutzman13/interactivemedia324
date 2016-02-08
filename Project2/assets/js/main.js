61a4718ba6d3e86548078e612b420f6be5a75dcf
tstutzman13 <tstutzman13@wou.edu>
2016-02-08 08:43:36 -0800

    Project 2

---
+++ Project2/assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready( function () {
+
+  var rathers = [
+    "eat ramen for 100 days",
+    "write a 10 page research paper",
+    "code a 15 page website",
+  ]
+
+
+  $('.yellow').click(function(){
+    $('.red').toggle();
+
+  });
+
+
+
+
+//DO NOT DELEATE BLEOW THIS POINT
+});
