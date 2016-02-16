37ddbc469ba13f852ec7c4e650a08b8c92f12519
tstutzman13 <tstutzman13@wou.edu>
2016-02-15 16:45:05 -0800

    project2

--- Project2/assets/js/main.js
+++ Project2/assets/js/main.js
@@ -1,20 +1,33 @@
 $(document).ready( function () {
 
-  var rathers = [
-    "eat ramen for 100 days",
-    "write a 10 page research paper",
-    "code a 15 page website",
-  ]
 
 
-  $('.yellow').click(function(){
-    $('.red').toggle();
+	function myFunction() {
+    	var x = Math.floor((Math.random() * rathers.length));
+    	document.getElementById("demo").innerHTML = x;
+	}
 
-  });
+	var rathers = [
+    	"eat top ramen for 100 days",
+    	"write a 10 page research paper",
+    	"code a 15 page website",
+  	]
 
-console.log("Hello");
+	$(function() {
+	    while( $('#right div').height() > $('#right').height() ) {
+	        $('#right div').css('font-size', (parseInt($('#right div').css('font-size')) - 1) + "px" );
+	    }
+	});
 
+  //$('#left').click(function(){
+    //myFunction();
 
+  //});
+
+  //$('#red').click(function(){
+    //$('#yellow').toggle();
+
+  //});
 
 //DO NOT DELEATE BLEOW THIS POINT
 });
